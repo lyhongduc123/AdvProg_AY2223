14678b53de6aa993e50b5b40f88fde8f0e63c2e3
lyhongduc123 <22021217@vnu.edu.vn>
2023-02-15 13:58:37 +0700

    Completed the jump, rotate and color function. Passed the debug

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0);
 }
 
 
@@ -21,6 +22,8 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    x += (int)(numPixel * cos(angle));
+    y += (int)(numPixel * -sin(angle));
 }
 
 
@@ -33,6 +36,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    jumpForward(-numPixel);
 }
 
 
@@ -44,7 +48,8 @@ void Painter::jumpBackward(int numPixel)
 ***/       
 void Painter::turnLeft(double degree)
 {
-    // TODO: rotate left the painter   
+    // TODO: rotate left the painter
+    angle += degree * M_PI / 180;
 }
 
 
@@ -56,7 +61,8 @@ void Painter::turnLeft(double degree)
 ***/     
 void Painter::turnRight(double degree)
 {
-    // TODO: rotate right the painter   
+    // TODO: rotate right the painter
+    angle -= degree * M_PI / 180;
 }
 
 /***  
@@ -67,7 +73,12 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color 
+    color.r = rand() % 256 + 0;
+    color.g = rand() % 256 + 0;
+    color.b = rand() % 256 + 0;
+    color.a = 0;
+    setColor(color);
 }
 
 
